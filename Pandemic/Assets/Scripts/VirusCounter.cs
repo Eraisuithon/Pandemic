c9998e790b5acefdaaf8ca4d445f293846572007
Mike Roditis <45882819+Eraisuithon@users.noreply.github.com>
2022-08-10 11:07:45 +0300

    Counter on Viruses
    There is a counter that shows the available viruses

--- Pandemic/Assets/Scripts/VirusCounter.cs
+++ Pandemic/Assets/Scripts/VirusCounter.cs
@@ -13,13 +13,21 @@ public class VirusCounter : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if (gameObject.name == "RedCounter")
+        if (name == "RedCounter")
             GetComponent<TextMesh>().text = GetComponentInParent<City>().RedCounter.ToString();
-        if (gameObject.name == "BlueCounter")
+        else if (name == "BlueCounter")
             GetComponent<TextMesh>().text = GetComponentInParent<City>().BlueCounter.ToString();
-        if (gameObject.name == "YellowCounter")
+        else if (name == "YellowCounter")
             GetComponent<TextMesh>().text = GetComponentInParent<City>().YellowCounter.ToString();
-        if (gameObject.name == "BlackCounter")
+        else if (name == "BlackCounter")
             GetComponent<TextMesh>().text = GetComponentInParent<City>().BlackCounter.ToString();
+        else if (transform.parent.name == "RedVirus")
+            GetComponent<TextMesh>().text = Board.redVirusAvailable.ToString() + 'x';
+        else if (transform.parent.name == "BlueVirus")
+            GetComponent<TextMesh>().text = Board.blueVirusAvailable.ToString() + 'x';
+        else if (transform.parent.name == "YellowVirus")
+            GetComponent<TextMesh>().text = Board.yellowVirusAvailable.ToString() + 'x';
+        else if (transform.parent.name == "BlackCounter")
+            GetComponent<TextMesh>().text = Board.blackVirusAvailable.ToString() + 'x';
     }
 }
